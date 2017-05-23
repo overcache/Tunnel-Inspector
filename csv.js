1b16fc6b64e573142e28522383875e6e6698cbc4
icymind <icymindt@gmail.com>
2017-05-23 21:43:49 +0800

    auto detect encoding

--- csv.js
+++ csv.js
@@ -1,12 +1,32 @@
 const fs = require("fs")
-const readline = require("readline")
 const lineReader = require("line-reader")
 const parse = require("csv-parse")
 const parseSync = require("csv-parse/lib/sync")
-// const sqlite3 = require("sqlite3").verbose()
 const assert = require("assert")
-// const { expect } = require("chai")
+const jschardet = require("jschardet")
+const iconv = require("iconv-lite")
 
+
+function detectEncoding(file) {
+  return new Promise((resolve, reject) => {
+    const bufferSize = 128 * 1024
+    const buffer = new Buffer(bufferSize)
+    fs.open(file, "r", (err, fd) => {
+      if (err) {
+        console.log(err)
+        reject(err)
+      }
+      fs.read(fd, buffer, 0, bufferSize, null, (err, bytesRead, buffer) => {
+        if (err) {
+          reject(err)
+        }
+        fs.close(fd, (err) => {
+          resolve(jschardet.detect(buffer).encoding)
+        })
+      })
+    })
+  })
+}
 function test() {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -24,6 +44,15 @@ function close(db) {
       }
       resolve()
     })
+  })
+}
+
+function finalizePromise(stmt) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      stmt.finalize()
+      resolve()
+    }, 5000)
   })
 }
 
@@ -60,22 +89,34 @@ function all(db, sql) {
   })
 }
 
-function promiseParse(line) {
-  return new Promise((resolve, reject) => {
-    parse(line, (err, row) => {
-      if (err) {
-        console.log(err)
-        // reject(err)
-      }
-      resolve(row)
-    })
-  })
+function split(str) {
+  if (str.indexOf("\n") >= 0) {
+    return str.split("\n")[0]
+  }
+  const index = str.indexOf("/")
+  if (index < 0) {
+    return str
+  }
+  // demo-L2/L3-master/demo-L2/L3-master
+  // demo-L2/L3-master
+  const pre = str.substring(0, index)
+  const nextIndex = str.indexOf(pre, index)
+  if (nextIndex < 0) {
+    return str
+  }
+  return str.substring(0, nextIndex - 1)
 }
 
-function createTunnelsTable(db) {
+function createTunnelsTable(db, type = "lte") {
+  let tableName
+  if (type === "lte") {
+    tableName = "lte_tunnels"
+  } else {
+    tableName = "non_lte_tunnels"
+  }
   return new Promise((resolve) => {
     db.serialize(() => {
-      const stmt = String.raw`CREATE TABLE IF NOT EXISTS "tunnels" (
+      const stmt = String.raw`CREATE TABLE IF NOT EXISTS "${tableName}" (
                   "id" integer primary key autoincrement not null,
                   "t_id" text,
                   "name" text not null,
@@ -87,16 +128,22 @@ function createTunnelsTable(db) {
                   "middle_in_ports" text not null,
                   "middle_out_ports" text not null
                 );`
-      db.run("drop table if exists tunnels")
+      db.run(`drop table if exists ${tableName}`)
       db.run(stmt, resolve)
     })
   })
 }
 
-function createBusinessesTable(db) {
+function createBusinessesTable(db, type = "lte") {
+  let tableName
+  if (type === "lte") {
+    tableName = "lte_businesses"
+  } else {
+    tableName = "non_lte_businesses"
+  }
   return new Promise((resolve) => {
     db.serialize(() => {
-      const stmt = String.raw`CREATE TABLE IF NOT EXISTS businesses (
+      const stmt = String.raw`CREATE TABLE IF NOT EXISTS "${tableName}" (
             "id" integer primary key autoincrement,
             "b_id" text,
             "name" text not null,
@@ -106,78 +153,80 @@ function createBusinessesTable(db) {
             "work_tunnel" text not null,
             "guard_tunnel" text not null
           );`
-      db.run("drop table if exists businesses")
+      db.run(`drop table if exists ${tableName}`)
       db.run(stmt, resolve)
     })
   })
 }
 
-function extractTunnels(db, file, callback) {
+async function extractTunnels(db, file, type, callback) {
   const tunnelPatten = /^[是|否],[是|否]?,\d*?,.*?,\d+?,[单|双]向,/i
   const header = "导入网管*,是否反向业务*,OID,Tunnel 名称*,Tunnel ID*,业务方向*,静态 CR Tunnel参数模板*,备注,网元*,端口,标签*,Tunnel接口,绑定到Tunnel策略,下一跳,网元*,端口,标签*,反向Tunnel接口,反向下一跳,自动计算路由*,约束粒度,约束类型,约束节点,网元,入端口,入标签,出端口,出标签,下一跳,Tunnel源节点 Tunnel OAM模板名称,Tunnel宿节点Tunnel OAM模板名称,OAM反向Tunnel,Tunnel源节点 Tunnel TPOAM模板名称,Tunnel宿节点Tunnel TPOAM模板名称,导入结果"
-  const stmt = db.prepare("insert into tunnels (t_id, name, src_element, src_port, dest_element, dest_port, middle_elements, middle_in_ports, middle_out_ports) values(?,?,?,?,?,?,?,?,?)")
+  const stmt = db.prepare(`insert into ${type}_tunnels (t_id, name, src_element, src_port, dest_element, dest_port, middle_elements, middle_in_ports, middle_out_ports) values(?,?,?,?,?,?,?,?,?)`)
+  const encoding = await detectEncoding(file)
+  if (encoding === null) {
+    throw new Error("can not detect file's encoding")
+  }
 
-  lineReader.eachLine(file, { separator: "\r\n", encoding: "utf8" }, async (line, last) => {
+  lineReader.eachLine(file, { separator: "\r\n", encoding: "binary" }, async (raw, last) => {
+    const line = iconv.decode(Buffer.from(raw, "binary"), encoding)
     if (tunnelPatten.test(line)) {
       parse(line, (err, output) => {
         const value = output[0]
         if (value) {
-          stmt.run([value[4], value[3], value[8],
+          stmtRun(db, stmt, [value[4], value[3], value[8],
             value[9], value[14], value[15], value[23], value[24], value[26]])
         }
       })
     }
     if (last) {
-      setTimeout(() => {
-        stmt.finalize(callback)
-      }, 5000)
+      await finalizePromise(stmt)
+      callback()
     }
   })
 }
 
-function extractTunnelsPromise(db, file) {
+function extractTunnelsPromise(db, file, type) {
   return new Promise((resolve, reject) => {
-    extractTunnels(db, file, resolve)
+    extractTunnels(db, file, type, resolve)
   })
 }
 
-function extractBusinesses(db, file, callback) {
+async function extractBusinesses(db, file, callback) {
   const workTunnelPatten = /^[是|否],[0|1],.*?,.*?,\d*?,.*?,.*?,.+?,.*?,工作,/i
   const guardTunnelPatten = /^,{9}保护,/i
   const header = "导入网管*,是否反向业务*,OID,业务名称*,业务ID,客户名称,承载业务类型,模板名称*,保护类型*,,源站点,网元*,端口*,端口描述,子接口ID,VLAN ID,Uni Qos Policy,业务分界标签,源优先级类型,源优先级域,网元*,端口*,端口描述,子接口ID,VLAN ID,Uni Qos Policy,业务分界标签,宿优先级类型,宿优先级域,左网元*,右网元*,PW ID*,PW标签,Tunnel类型*,Tunnel 名称,PW Qos Policy,PW模板,管理PW,保护模板名称,左网元,右网元,PW ID,PW标签,Tunnel类型,Tunnel 名称,PW Qos Policy,PW模板,管理PW,保护类型,源保护组ID,宿保护组ID,备注,描述,客户业务类型,区域,定制属性1,定制属性2,Y.1731 TP OAM模板,Y.1711 OAM模板,BFD,导入结果"
 
-  const stmt = db.prepare("insert into businesses (b_id, name, src_port, work_dest_port, guard_dest_port, work_tunnel, guard_tunnel) values(?,?,?,?,?,?,?)")
+  const stmt = db.prepare("insert into lte_businesses (b_id, name, src_port, work_dest_port, guard_dest_port, work_tunnel, guard_tunnel) values(?,?,?,?,?,?,?)")
+  const encoding = await detectEncoding(file)
+  if (encoding === null) {
+    throw new Error("can not detect file's encoding")
+  }
   let record = null
 
-  lineReader.eachLine(file, { separator: "\r\n", encoding: "utf8" }, async (line, last) => {
+  lineReader.eachLine(file, { separator: "\r\n", encoding: "binary" }, async (raw, last) => {
+    const line = iconv.decode(Buffer.from(raw, "binary"), encoding)
     if (workTunnelPatten.test(line)) {
       const [value] = parseSync(line)
       assert.equal(value === undefined, false)
-      console.log(record)
       assert.equal(record, null)
       record = {}
       record.b_id = value[4]
       record.name = value[3]
       record.src_port = value[12]
       record.work_dest_port = value[21]
-      record.work_tunnel = value[34].split(/[\n|/]/)[0]
+      record.work_tunnel = split(value[34])
     } else if (guardTunnelPatten.test(line)) {
       const [value] = parseSync(line)
       assert.equal(value === undefined, false)
       assert.equal(record === null, false)
 
       record.guard_dest_port = value[21]
-      record.guard_tunnel = value[34].split(/[\n|/]/)[0]
+      record.guard_tunnel = split(value[34])
       await stmtRun(db, stmt, [record.b_id, record.name, record.src_port,
         record.work_dest_port, record.guard_dest_port,
         record.work_tunnel, record.guard_tunnel])
       record = null
-      // db.serialize(() => {
-        // stmt.run([record.b_id, record.name, record.src_port,
-          // record.work_dest_port, record.guard_dest_port,
-          // record.work_tunnel, record.guard_tunnel])
-        // record = null
-      // })
     }
     if (last) {
       setTimeout(() => {
@@ -197,6 +246,81 @@ function common(workRoute, guardRoute) {
   const work = workRoute.split("\n")
   const guard = guardRoute.split("\n")
   return work.filter(route => guard.includes(route)).join("\n")
+}
+
+function fillRow(record, type) {
+  let TName
+  let TSrcElement
+  let TSrcPort
+  let TDestElement
+  let TDestPort
+  let BDestPort
+  let TMiddleElements
+  let TMiddleInPorts
+  let TMiddleOutPorts
+
+  if (type === "工作") {
+    TName = record.work_name
+    TSrcElement = record.work_src_element
+    TSrcPort = record.work_src_port
+    TDestElement = record.work_dest_element
+    TDestPort = record.work_dest_port
+    BDestPort = record.b_work_desk_port
+    TMiddleElements = record.work_middle_elements.split("\n")
+    TMiddleInPorts = record.work_middle_in_ports.split("\n")
+    TMiddleOutPorts = record.work_middle_out_ports.split("\n")
+  } else {
+    TName = record.guard_name
+    TSrcElement = record.guard_src_element
+    TSrcPort = record.guard_src_port
+    TDestElement = record.guard_dest_element
+    TDestPort = record.guard_dest_port
+    BDestPort = record.b_guard_desk_port
+    TMiddleElements = record.guard_middle_elements.split("\n")
+    TMiddleInPorts = record.guard_middle_in_ports.split("\n")
+    TMiddleOutPorts = record.guard_middle_out_ports.split("\n")
+  }
+  const result = []
+  result.push(record.b_name)
+  result.push(type)
+  result.push(`${record.b_src_element}#${record.b_src_port}`)
+  result.push(`${TDestElement}#${BDestPort}`)
+  result.push(`${TName}`)
+  const segments = []
+  const routes = []
+  segments.push(`${TSrcElement}#${TSrcPort}`)
+  for (let i = 0, len = TMiddleInPorts.length; i < len; i += 1) {
+    segments.push(`${TMiddleElements[i]}#${TMiddleInPorts[i]}`)
+    segments.push(`${TMiddleElements[i]}#${TMiddleOutPorts[i]}`)
+  }
+  segments.push(`${TDestElement}#${TDestPort}`)
+  for (let i = 1, len = segments.length; i < len; i += 2) {
+    routes.push(`${segments[i - 1]} <===> ${segments[i]}`)
+  }
+  result.push(routes.join("\n"))
+  return result
+}
+
+function mergeRow(row) {
+  const work = fillRow(row, "工作")
+  const guard = fillRow(row, "保护")
+  const workRoute = work[work.length - 1]
+  const guardRoute = guard[guard.length - 1]
+  const inCommon = common(workRoute, guardRoute)
+  work.push(inCommon)
+  guard.push(inCommon)
+  // if (inCommon) {
+    // console.log("=================================\nrow:")
+    // console.log(work)
+    // console.log(guard)
+    // console.log("*********************************\nwork:")
+    // console.log(work[work.length - 2])
+    // console.log("*********************************\nguard:")
+    // console.log(guard[guard.length - 2])
+    // console.log("*********************************\ncommon:")
+    // console.log(inCommon)
+  // }
+  return [work, guard]
 }
 
 function mergeToOutput(business, workTunnel, guardTunnel) {
@@ -255,23 +379,23 @@ function print(id, result) {
 }
 
 async function inspect(db, id) {
-  const business = await get(db, `select * from businesses where id = ${id}`)
-  const workTunnels = await all(db, `select * from tunnels where name = "${business.work_tunnel}"`)
+  const business = await get(db, `select * from lte_businesses where id = ${id}`)
+  const workTunnels = await all(db, `select * from lte_tunnels where name = "${business.work_tunnel}"`)
   let workTunnel = null
   let guardTunnel = null
   if (workTunnels.length < 1) {
-    throw new Error(`no workTunnel for business: id: ${id}`)
+    throw new Error(`no workTunnel for business: id: ${id}, name: ${business.name}, workTunnel: ${business.work_tunnel}`)
   } else if (workTunnels.length > 1) {
-    throw new Error(`more than 1 workTunnel for business: id: ${id}`)
+    throw new Error(`more than one workTunnel for business: id: ${id}, name: ${business.name}, workTunnel: ${business.work_tunnel}`)
   } else {
     workTunnel = workTunnels[0]
   }
 
-  const guardTunnels = await all(db, `select * from tunnels where name = "${business.guard_tunnel}"`)
+  const guardTunnels = await all(db, `select * from lte_tunnels where name = "${business.guard_tunnel}"`)
   if (guardTunnels.length < 1) {
-    throw new Error(`no guardTunnel for business: id: ${id}`)
+    throw new Error(`no guardTunnel for business: id: ${id}, name: ${business.name}, guardTunnel: ${business.guard_tunnel}`)
   } else if (guardTunnels.length > 1) {
-    throw new Error(`more than 1 guardTunnel for business id: ${id}`)
+    throw new Error(`more than one guardTunnel for business: id: ${id}, name: ${business.name}, guardTunnel: ${business.guard_tunnel}`)
   } else {
     guardTunnel = guardTunnels[0]
   }
@@ -292,4 +416,5 @@ module.exports = {
   inspect,
   test,
   close,
+  mergeRow,
 }
--- test/csv.test.js
+++ test/csv.test.js
@@ -5,8 +5,8 @@ const path = require("path")
 const sqlite3 = require("sqlite3").verbose()
 const csv = require(path.join(__dirname, "../csv.js"))
 const dbpath =path.join(__dirname, "aa.db")
-const tunnelsPath = path.join(__dirname, "tunnels-all.csv")
-const ltePath = path.join(__dirname, "lte-all.csv")
+const tunnelsPath = path.join(__dirname, "CMCC/LTE业务Tunnel信息表.csv")
+const ltePath = path.join(__dirname, "CMCC/LTE业务信息表.csv")
 
 
 describe("Test module csv", function () {
@@ -14,28 +14,33 @@ describe("Test module csv", function () {
   this.timeout(300000)
   before(async function () {
     const db = new sqlite3.Database(dbpath)
-    await csv.createTunnelsTable(db)
-    await csv.createBusinessesTable(db)
-    await csv.extractTunnelsPromise(db, tunnelsPath)
-    await csv.extractBusinessesPromise(db, ltePath)
+    await csv.createTunnelsTable(db, "lte")
+    await csv.createBusinessesTable(db, "lte")
+    await csv.extractTunnelsPromise(db, tunnelsPath, "lte")
+    try {
+      await csv.extractBusinessesPromise(db, ltePath)
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
     await csv.close(db)
   })
 
   it("total tunnels records", async function () {
     const db = new sqlite3.Database(dbpath)
-    const { count } = await csv.get(db, "select count(*) as count from tunnels")
+    const { count } = await csv.get(db, "select count(*) as count from lte_tunnels")
     expect(count).to.be.equal(17225)
   })
 
   it("total businesses records", async function () {
     const db = new sqlite3.Database(dbpath)
-    const { count } = await csv.get(db, "select count(*) as count from businesses")
+    const { count } = await csv.get(db, "select count(*) as count from lte_businesses")
     expect(count).to.be.equal(9586)
   })
 
   it("extractTunnels", async function () {
     const db = new sqlite3.Database(dbpath)
-    const row = await csv.get(db, "select * from tunnels where t_id = '32213'")
+    const row = await csv.get(db, "select * from lte_tunnels where t_id = '32213'")
     expect(row.t_id).to.be.equal("32213")
     expect(row.name).to.be.equal("231-白沙核心调度环2-2-11602-南宁隆安古潭中真福乍LTE")
     expect(row.src_element).to.be.equal("231-白沙核心调度环2-2")
@@ -50,7 +55,7 @@ describe("Test module csv", function () {
 
   it("extractBusinesses", async function () {
     const db = new sqlite3.Database(dbpath)
-    const row = await csv.get(db, "select * from businesses where b_id = '371764'")
+    const row = await csv.get(db, "select * from lte_businesses where b_id = '371764'")
     expect(row.b_id).to.be.equal("371764")
     expect(row.name).to.be.equal("南宁青秀区东郊分局_HLH新-网管")
     expect(row.src_port).to.be.equal("4-8")
@@ -73,15 +78,90 @@ describe("Test module csv", function () {
     await csv.close(db)
   })
 
-  it("inspect all", async function () {
+  it.skip("inspect all", async function () {
     const db = new sqlite3.Database(dbpath)
-    const { count } = await csv.get(db, "select count(*) as count from businesses")
+    const { count } = await csv.get(db, "select count(*) as count from lte_businesses")
+    let total = 0
     for (let i = 1; i <= 100; i += 1) {
       try {
         await csv.inspect(db, i)
       } catch(error) {
-        console.log(error)
+        total += 1
+        console.log(error.message)
       }
     }
+    console.log(`total error: ${total}`)
+  })
+
+  it("inspect 南宁西乡塘区锦虹棉纺织公司LTE-网管", function (done) {
+    const sql = String.raw`select
+      b.name as b_name,
+      w.src_element as b_src_element,
+      b.src_port as b_src_port,
+      b.work_dest_port as b_work_desk_port,
+      b.guard_dest_port as b_guard_desk_port,
+      w.name as work_name,
+      w.src_element as work_src_element,
+      w.src_port as work_src_port,
+      w.dest_element as work_dest_element,
+      w.dest_port as work_dest_port,
+      w.middle_elements as work_middle_elements,
+      w.middle_in_ports as work_middle_in_ports,
+      w.middle_out_ports as work_middle_out_ports,
+      g.name as guard_name,
+      g.src_element as guard_src_element,
+      g.src_port as guard_src_port,
+      g.dest_element as guard_dest_element,
+      g.dest_port as guard_dest_port,
+      g.middle_elements as guard_middle_elements,
+      g.middle_in_ports as guard_middle_in_ports,
+      g.middle_out_ports as guard_middle_out_ports
+      from lte_businesses as b
+      inner join lte_tunnels as w
+        on b.work_tunnel = w.name
+      inner join lte_tunnels as g
+        on b.guard_tunnel = g.name
+      where b.name = "南宁西乡塘区锦虹棉纺织公司LTE-网管"
+      `
+
+    const result = [
+      [
+        "南宁西乡塘区锦虹棉纺织公司LTE-网管",
+        "工作",
+        "11011-南宁西乡塘区锦虹棉纺织公司LTE（IP微波）#1-2",
+        "236-608核心调度环1-1#Eth-Trunk1.924",
+        "11011-南宁西乡塘区锦虹棉纺织公司-236-608核心调度环1-1-608方向",
+        "11011-南宁西乡塘区锦虹棉纺织公司LTE（IP微波）#1-1 <===> 1298-南宁西乡塘区连畴村戏台岭站TD#2-2\n\
+1298-南宁西乡塘区连畴村戏台岭站TD#2-1 <===> 1297-南宁兴宁区高峰林场站TD（替换）#1-1\n\
+1297-南宁兴宁区高峰林场站TD（替换）#2-1 <===> 5800-南宁兴宁区邕武路106号红树林TD（室外）#2-1\n\
+5800-南宁兴宁区邕武路106号红树林TD（室外）#1-1 <===> 1294-南宁兴宁区内环装饰市场3站TD#1-1\n\
+1294-南宁兴宁区内环装饰市场3站TD#2-1 <===> 89-南宁电视塔汇聚环9#1-10\n\
+89-南宁电视塔汇聚环9#11-2 <===> 90-南宁608汇聚环9#7-2\n\
+90-南宁608汇聚环9#11-2 <===> 236-608核心调度环1-1#GigabitEthernet9/0/1",
+        "11011-南宁西乡塘区锦虹棉纺织公司LTE（IP微波）#1-1 <===> 1298-南宁西乡塘区连畴村戏台岭站TD#2-2",
+      ],
+      [
+        "南宁西乡塘区锦虹棉纺织公司LTE-网管",
+        "保护",
+        "11011-南宁西乡塘区锦虹棉纺织公司LTE（IP微波）#1-2",
+        "234-白沙核心调度环1-1#Eth-Trunk1.927",
+        "11011-南宁西乡塘区锦虹棉纺织公司-234-白沙核心调度环1-1-白沙方向",
+        "11011-南宁西乡塘区锦虹棉纺织公司LTE（IP微波）#1-1 <===> 1298-南宁西乡塘区连畴村戏台岭站TD#2-2\n\
+1298-南宁西乡塘区连畴村戏台岭站TD#1-1 <===> 1299-南宁西乡塘区连畴村2队站TD（兴宁）#2-1\n\
+1299-南宁西乡塘区连畴村2队站TD（兴宁）#1-1 <===> 1300-南宁西乡塘区皂角村2站TD#2-1\n\
+1300-南宁西乡塘区皂角村2站TD#1-1 <===> 1301-南宁西乡塘区北湖大板市场站TD#2-1\n\
+1301-南宁西乡塘区北湖大板市场站TD#1-1 <===> 88-南宁北郊汇聚环9#1-9\n\
+88-南宁北郊汇聚环9#7-2 <===> 87-南宁金棉楼汇聚环9#11-2\n\
+87-南宁金棉楼汇聚环9#7-2 <===> 86-南宁百万庄汇聚环9#11-2\n\
+86-南宁百万庄汇聚环9#7-2 <===> 234-白沙核心调度环1-1#GigabitEthernet14/0/1",
+        "11011-南宁西乡塘区锦虹棉纺织公司LTE（IP微波）#1-1 <===> 1298-南宁西乡塘区连畴村戏台岭站TD#2-2",
+      ],
+    ]
+    const db = new sqlite3.Database(dbpath)
+    db.each(sql, (err, row) => {
+      expect(csv.mergeRow(row)).to.be.deep.equal(result)
+    }, (err, total) => {
+      done()
+    })
   })
 })
