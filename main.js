379f6a2a5c2a6659d5f1f4e9648d33cadecb2e75
icymind <icymindt@gmail.com>
2017-05-26 04:16:38 +0800

    add jquery to dep

--- main.js
+++ main.js
@@ -7,15 +7,17 @@ let win
 function createWindow() {
   win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 700,
     minWidth: 500,
   })
-  // win.maximize()
+
+  // win.webContents.openDevTools()
   win.loadURL(url.format({
     pathname: path.join(__dirname, "index.html"),
     protocol: "file",
     slashes: true,
   }))
+
 
   win.on("closed", () => {
     win = null
